f4f90e092d77edee105e35d5a4ce2add69876b95
QuanNH97 <quannh220597@gmail.com>
2020-08-14 09:23:47 +0700

    Initial Commit

---
+++ AutoLayoutExercise2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutExercise2
+//
+//  Created by Quan Nguyen on 8/14/20.
+//  Copyright © 2020 quannh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
